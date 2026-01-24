589a6384b744b414fc6c580a1fc7b755c1f4e44c
kxenopoulou <katerinaxenopoulou@gmail.com>
2026-01-24 21:24:40 +0200

    Update covid_dialectical_system.py
    Signed-off-by: kxenopoulou <katerinaxenopoulou@gmail.com>

--- mathematical_formalization/dialectical_transitions/applications/covid_dialectics/covid_dialectical_system.py
+++ mathematical_formalization/dialectical_transitions/applications/covid_dialectics/covid_dialectical_system.py
@@ -1,1 +1,35 @@
+"""
+Διαλεκτικό Σύστημα Ανάλυσης COVID-19
+Μαθηματική τυποποίηση σύμφωνα με το έργο του Ξενόπουλου
+"""
 
+import numpy as np
+import pandas as pd
+from typing import Dict, List
+
+class COVIDDialecticalSystem:
+    """Τυποποίηση του EnhancedCOVIDDialecticalSystem"""
+    
+    def __init__(self):
+        self.propositional_matrix = None
+        self.truth_values = {}
+        self.contradictions = []
+        self.syntheses = []
+    
+    def create_propositional_matrix(self, data: pd.DataFrame):
+        """Δημιουργία προτασιακής μήτρας P(x,y,z,t) = [v, τ, σ]"""
+        matrix = []
+        for idx, row in data.iterrows():
+            proposition = {
+                'coordinates': (row['x'], row['y'], row['z'], row['t']),
+                'truth_value': self.calculate_truth_value(row),
+                'proposition_type': self.determine_proposition_type(row),
+                'process_stage': self.determine_process_stage(row)
+            }
+            matrix.append(proposition)
+        return matrix
+    
+    def calculate_truth_value(self, row) -> float:
+        """Υπολογισμός τιμής αλήθειας v ∈ {0, ½v, ½²v, ..., 1}"""
+        # Ο κώδικάς σου για adaptive normalization
+        pass
